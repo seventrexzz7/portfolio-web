c05e03fe7e32fa71a03ab22f550ed7ca4086a3a2
Mar Simó Alejos <astraproductionscontact@gmail.com>
2025-10-10 13:23:10 +0200

    responsive wip

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-const contactBtn = document.querySelector(".w-btn");
-const cvBtn = document.querySelector(".b-btn");
+document.addEventListener('DOMContentLoaded', () => {
+    const contactBtn = document.querySelector(".w-btn");
+    const cvBtn = document.querySelector(".b-btn");
 
-contactBtn.addEventListener("click", () =>{
-    document.querySelector("#contact").scrollIntoView();
-});
+    const toggle = document.getElementById('menu-toggle');
+    const nav = document.getElementById('nav-links');
 
-cvBtn.addEventListener("click", ()=> {
-    window.open("", "_blank");
+    toggle.addEventListener('click', () => {
+        nav.classList.toggle('active');
+    });
+
+    contactBtn.addEventListener("click", () =>{
+        document.querySelector("#contact").scrollIntoView();
+    });
+
+    cvBtn.addEventListener("click", ()=> {
+        window.open("", "_blank");
+    })
 })
